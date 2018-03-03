d8a9daa12e7f96a35a356cd0e0358353c46b1686
madman <yakovlev.m.r@gmail.com>
2018-03-03 21:08:32 +0300

    Add start handler

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -6,5 +6,7 @@ modules = glob.glob(dirname(__file__)+"/*.py")
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 sys.path.append('..')
 from run import bot,r
+from menus.MainMenu import MainMenu
+main_menu = MainMenu()
 
 
